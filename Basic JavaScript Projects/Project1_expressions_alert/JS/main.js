96c0cfa5e8ae847bec9c48a6304df78d358b1c71
andrew609-code <142052575+andrew609-code@users.noreply.github.com>
2023-09-14 09:06:17 -0700

    changes made

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+var x = ("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+
+var Family = "The Marshalls", Dad = "Steve", Mom = "Rita", Son = "Andrew", Cat = "None";
+
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+document.write(x);
+document.write(Cat);
