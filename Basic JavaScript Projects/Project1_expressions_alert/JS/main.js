281c70026bff66dcb983a27252dd44999e352d1c
andrew609-code <142052575+andrew609-code@users.noreply.github.com>
2023-09-14 10:24:06 -0700

    Basic JavaScript Projects

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,13 +1,32 @@
+
+//Window alert method
+window.alert('Press OK to continue !');
+
+//Concatenating a string
 var x = ("\"Be who you are and say what you feel,"
 + " because those who mind don\'t matter and those who matter don\'t mind.\""
 + "-Dr. Seuss");
 
+//Creating 2 variables
+var Sent1 = "This is the beginning of the string\,", Sent2 = "\ This is the end of the string";
+
+//Variable 
 var Family = "The Marshalls", Dad = "Steve", Mom = "Rita", Son = "Andrew", Cat = "None";
 
+//Display using breaks 
+document.write(Sent1, Sent2);
+document.write("<br/>");
+document.write(x);
+document.write("<br/>");
+document.write(Cat);
+document.write("<br/>");
+document.write(31 * 4);
+
+//My first function
 function My_First_Function() {
     var str = "This is the button text!";
     document.getElementById("Button_Text").innerHTML = str;
 }
 
-document.write(x);
-document.write(Cat);
+//I wrote it out a little different to utilize breaks, I can cut and paste it like the model is ?
+
