d7621cf4405c5c221a06c69c5a72654a1fb3e3de
andrew609-code <142052575+andrew609-code@users.noreply.github.com>
2023-09-19 19:27:33 -0700

    project5

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/Example.js
@@ -0,0 +1,11 @@
+function my_Function() {
+    document.getElementById("Test").innerHTML = isNaN('This is a string');
+}
+function my_Function2() {
+    document.getElementById("Test2").innerHTML = isNaN('007');
+}
+document.write(2E310);
+document.write(-3E310);
+document.write(10 > 2);
+document.write(10 < 2);
+console.log(2 < 1);
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,48 @@
+// 2 numbers of same type and value using === "equal to and data type"
+x = 10;
+y = 10;
+document.write(x === y);
+document.write("<br>");
+
+// Combines a string with a number and a number
+a = 82;
+b = "82";
+document.write(a === b);
+document.write("<br>");
+
+// Combines a string with a word and a number
+c = "Ten";
+d = 10;
+document.write(c === d);
+document.write("<br>");
+
+// Combines the same type but different values
+e = 23;
+f = 46;
+document.write(e === f);
+document.write("<br>");
+
+// And Statements resulting in true and false
+document.write(5 > 2 && 10 > 4);
+document.write("<br>");
+document.write(5 < 2 && 10 > 4);
+document.write("<br>");
+
+// Or Statements resulting in true and false
+document.write(5 > 2 || 3 > 5);
+document.write("<br>");
+document.write(1 > 2 || 2 > 3);
+
+// Not function with onclick resulting in true and false
+function not_Function () {
+    document.getElementById ("Not").innerHTML = !(20 > 10);
+}
+
+function not_Function2 () {
+    document.getElementById ("Not2").innerHTML = !(10 > 20);
+}
+document.write("<br>");
+
+// Utilizing the == operator "equal to"
+document.write (5 == 5);
+
